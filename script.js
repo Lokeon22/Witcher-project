2ffdf40516619f1c3e09e72ce06538564efdc550
Gabriel Felipe <gabrielfelipe941@yahoo.com.br>
2022-05-16 20:06:14 -0300

    witcher project update coming

---
+++ script.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector(".menu-icon");
+let title = document.querySelector(".title-content");
+
+function sumir() {
+  setTimeout(() => {
+    title.classList.add("fadi");
+  }, 1000);
+}
+
+menu.addEventListener("click", () => {
+  let show = document.querySelector(".showcase");
+  let social = document.querySelector(".social-mobile");
+  menu.classList.toggle("active");
+  show.classList.toggle("active");
+  social.classList.toggle("active");
+});
+
+window.onload = sumir();
