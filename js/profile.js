31985b36fd19a56de2ec22c7b4ea53131181fe30
Gabriel Felipe <gabrielfelipe941@yahoo.com.br>
2022-05-18 19:47:46 -0300

    profile-cards js updated

--- js/profile.js
+++ js/profile.js
@@ -1,9 +1,26 @@
 let profile = document.querySelector(".title-profile");
 
+function cards() {
+  let lista = document.querySelectorAll(".names-profiles a");
+  lista = Array.from(lista);
+
+  const dataSpecial = document.querySelectorAll("[data-special]");
+  const dataSpecialArray = Array.from(dataSpecial);
+
+  lista.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      dataSpecialArray.forEach((div_item) => {
+        div_item.classList.add("active-profile");
+      });
+      dataSpecialArray[index].classList.toggle("active-profile");
+    });
+  });
+}
+
 function aparecer() {
   setTimeout(() => {
     profile.classList.add("fadi-profile");
   }, 1000);
 }
 
-window.onload = aparecer();
+(window.onload = aparecer()), cards();
